a9bd3f5589c0e0e4eed3428f74505c0bde5072dd
Makoto Yamazaki <m_yamazaki@yumemi.co.jp>
2021-12-18 17:09:18 +0900

    WIP

--- app/src/main/java/org/zakky/tsumipla/MainActivity.kt
+++ app/src/main/java/org/zakky/tsumipla/MainActivity.kt
@@ -3,41 +3,73 @@ package org.zakky.tsumipla
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import org.zakky.tsumipla.ui.theme.TsumiPlaTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TsumiPlaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val allScreens = RootScreen.values().toList()
+                val navController = rememberNavController()
+                val backstackEntry = navController.currentBackStackEntryAsState()
+                val currentScreen = RootScreen.fromRoute(backstackEntry.value?.destination?.route)
+
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = allScreens,
+                            currentScreen = currentScreen,
+                            onTabSelected = { screen ->
+                                navController.navigate(screen.name)
+                            },
+                        )
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = RootScreen.Main.name,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        RootScreen.values().forEach { screen ->
+                            composable(screen.name) {
+                                Text("${screen.name} screen")
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+enum class RootScreen {
+    Main,
+    Search,
+    ;
+
+    companion object {
+        fun fromRoute(route: String?): RootScreen = values().let { screens ->
+            screens.find { it.name == route } ?: screens[0]
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TsumiPlaTheme {
-        Greeting("Android")
-    }
+fun BottomNavigationBar(
+    items: List<RootScreen>,
+    currentScreen: RootScreen,
+    onTabSelected: (screenName: RootScreen) -> Unit,
+) {
+    Text("nav bar")
 }
